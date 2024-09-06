53abd5878d0675723a5556177ecfe1fa5852adc6
VelascoRt <velascogmac@gmail.com>
2024-09-06 01:24:39 -0600

    "1.0 - ARCHIVOS BASE"

---
+++ app/src/main/java/com/example/heroesapp/models/User.kt
@@ -0,0 +1,15 @@
+package com.example.heroesapp.models
+
+data class User(val name: String, val email: String, val password: String) {
+    val computedName : String get() = "Bienvenido $name"
+    companion object{
+        val users = listOf(
+            User("Victor","victor@hotmail.com","contraseña"),
+            User("Samuel","samu1321@hotmail.com","pass"),
+            User("Jonathan","dipshelby@hotmail.com","word"),
+            User("Leonardo","leorambo@hotmail.com","ordwassp"),
+            User("Juan","juanfr97@hotmail.com","12345"),
+            User("Daniel","daniel98@hotmail.com","12345")
+        )
+    }
+}
