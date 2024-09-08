c0609826689cc15d444e6ca0d76469514609186f
VelascoRt <velascogmac@gmail.com>
2024-09-08 14:53:51 -0600

    "1.1 - LOGIN FUNCIONAL"

--- app/src/main/java/com/example/heroesapp/MainActivity.kt
+++ app/src/main/java/com/example/heroesapp/MainActivity.kt
@@ -1,20 +1,56 @@
 package com.example.heroesapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.heroesapp.activities.HomeActivity
+import com.example.heroesapp.models.User
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    // Valores login
+    lateinit var emailEditText : EditText
+    lateinit var passwordEditTExt : EditText
+    lateinit var  loginBtn : Button
+
+    // Instancia
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // Valores login
+        emailEditText = findViewById(R.id.emailET)
+        passwordEditTExt = findViewById(R.id.passwordET)
+        loginBtn = findViewById(R.id.btnLogin)
+        loginBtn.setOnClickListener { v ->
+            val email = emailEditText.text.toString()
+            val password = passwordEditTExt.text.toString()
+            // Login incorrecto, valores vacios.
+            if (email.isEmpty() || password.isEmpty()) {
+                Snackbar.make(v,"Correo o contraseña incorrectos",Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            // Verificar correo y contraseña.
+            val isValidUser = User.users.any {
+                user -> user.email == email && user.password == password
+            }
+            // Login incorrecto.
+            if (!isValidUser) {
+                Snackbar.make(v,"Correo o contraseña incorrectos",Snackbar.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            // Login Succesful, ingresar a HOME.
+            val loginSuccesful = Intent(this@MainActivity,HomeActivity::class.java)
+            startActivity(loginSuccesful)
+
         }
     }
 }
--- app/src/main/java/com/example/heroesapp/models/User.kt
+++ app/src/main/java/com/example/heroesapp/models/User.kt
@@ -4,7 +4,7 @@ data class User(val name: String, val email: String, val password: String) {
     val computedName : String get() = "Bienvenido $name"
     companion object{
         val users = listOf(
-            User("Victor","victor@hotmail.com","contraseña"),
+            User("Victor","victor@hotmail.com","contrasea"),
             User("Samuel","samu1321@hotmail.com","pass"),
             User("Jonathan","dipshelby@hotmail.com","word"),
             User("Leonardo","leorambo@hotmail.com","ordwassp"),
